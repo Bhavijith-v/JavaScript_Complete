abf2023e175836d71bec0bff2e58cc063b7eda09
bhavijithvijayakumar777@gmail.com <bhavijithv@Bhavijiths-MacBook-Air.local>
2025-05-08 16:19:44 +0530

    data type updated

--- Data-Types/data-type.js
+++ Data-Types/data-type.js
@@ -1,2 +1,34 @@
 let number  = 7;
 console.log(typeof number); // Output: number
+
+let string = "Hello, World!";
+console.log(typeof string); // Output: string
+
+let boolean = true;
+console.log(typeof boolean); // Output: boolean
+
+let array = [1, 2, 3, 4, 5];
+console.log(typeof array); // Output: object
+
+let object = { name: "John", age: 30 };
+console.log(typeof object); // Output: object
+
+let nullValue = null;
+console.log(typeof nullValue); // Output: object
+
+let undefinedValue;
+console.log(typeof undefinedValue); // Output: undefined
+
+
+let symbolValue = Symbol("My Symbol");
+console.log(typeof symbolValue); // Output: symbol
+
+let functionValue = function() {};
+console.log(typeof functionValue); // Output: function
+
+let bigIntValue = BigInt(9007199254740991);
+
+
+console.log(typeof bigIntValue); // Output: bigint
+
+
